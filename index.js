b32a849da613db7fc2d7f069faa98817de368520
Stephen Buley <stephen.a.buley@gmail.com>
2024-03-04 00:03:02 +0100

    add options and argument to program

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
+import { program } from 'commander'
+
+program
+  .option('-c, --camel-case', 'change words in a filename to camelCase')
+  .option('-s, --snake-case', 'change words in a filename to snake_case')
+  .option('-k, --kebab-case', 'change words in a filname to kebab-case')
+  .argument('<filename>', 'the path to the file name that you want to change')
+
+program.parse()
+
+const options = program.opts()
+
 console.log('hello from node')
+console.dir(options)
